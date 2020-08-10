4594be6f94f693aaa073bd812029205913af0c5a
Ben <benjaminkyd@gmail.com>
2020-08-10 19:31:37 +0100

    hotfix - bug in the bloody message edit event

--- src/guild-controller.js
+++ src/guild-controller.js
@@ -1,10 +1,10 @@
 const Database = require('./database.js')
 
-module.exports.SetupGuild = async function(guild)
+module.exports.SetupGuild = async function(id, name, logchannel = -1, )
 {
 
     const GuildSettings = {
-        
+        DoUnpingableNickname: false
     };
 
     Database.NewGuild();
